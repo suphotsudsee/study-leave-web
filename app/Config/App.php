8050aea571257446bcae4e35676427003f89d784
suphotsudsee <suphotsudsee@gmail.com>
2026-01-07 18:32:39 +0700

    fff

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://study-leave-web.phoubon.in.th/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
